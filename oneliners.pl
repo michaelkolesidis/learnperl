27ac9ec545c7eda7f70d882e751d062271ccddb7
Michael Kolesidis <michael.kolesidis@gmail.com>
2022-06-01 23:53:55 +0300

    Update oneliners.pl

--- oneliners.pl
+++ oneliners.pl
@@ -1,1 +1,19 @@
-# One-Liners in Perl
+# USEFUL ONE-LINERS IN PERL
+
+# 1
+# Change string found in file to string provided
+
+perl -pi -e 's/lookForThisString/ChangeItToThisString/g' sample.txt
+
+# The above code will look for the string "lookForThisString" in a sample.txt file 
+# and change it to "ChangeItToThisString". Runs in Unix and Unix-Like systems.
+
+
+
+# 2
+#
+
+perl
+
+#
+#
